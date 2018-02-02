3de9ab1a2d1c8a6cca2252ae48289a6b893a7d6f
cravingdata <32245139+cravingdata@users.noreply.github.com>
2018-02-01 20:46:23 -0500

    Add files via upload

---
+++ finding_hypotenuse.py
@@ -0,0 +1,6 @@
+a = 12  # length of triangle side a
+b = 10  # lenght of triangle side b
+
+if a != 0 or b != 0:
+    c = ((a ** 2) + (b ** 2)) ** 0.5   # finding square root of a + b
+print c         # printing hypotenuse
